59999bcedeabf60c62122143bdd841dd04062712
rimfat1311 <simontimnan@gmail.com>
2024-09-01 14:55:29 -0700

    update on src

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import Navbar from "./components/Navbar";
+
 export default function App() {
   return (
     <h1 className="text-3xl font-bold underline">
+      <Navbar />
       Hello world!
     </h1>
   )
