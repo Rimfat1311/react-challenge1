dcb0436335fb4673b16807284e3751ff90bfbf3c
rimfat1311 <simontimnan@gmail.com>
2024-09-01 16:38:10 -0700

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='flex p-10 justify-between'>
+    <div className='flex p-10 justify-between text-pink-700'>
         <h1>The Dojo Blog</h1>
         <div>
-            <ul className='flex justify-between gap-10'>
+            <ul className='flex gap-10'>
                 <li><a href="">Home</a></li>
                 <li><a href="">New Blog</a></li>
             </ul>
