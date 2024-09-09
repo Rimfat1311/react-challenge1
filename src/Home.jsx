48c0bc8be355bf305d90eee780e516720a7342df
rimfat1311 <simontimnan@gmail.com>
2024-09-09 08:38:56 -0700

    update

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
+import { useState } from 'react';
 
 const Home = () => {
+    const [name, setName] = useState("Timnan")
+    const [age, setAge] = useState(30)
+
     const handleClick = () => {
-        console.log('hello timnan');
+        setName("Rimfat")
+        setAge(35)
       }
     
    
   return (
     <div>
-        <h2>Homepage</h2>
+        <h2 className='font-bold'>Homepage</h2>
+        <p className='m-5'> {name} is {age} years old </p>
         <button onClick={handleClick} className='bg-orange-500 p-5 hover:bg-slate-400'>click me</button>
-   
         </div>
   )
 }
