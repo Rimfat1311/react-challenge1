d594e4a32f6fc38d9c0ae34ae1a12a8ee9854639
rimfat1311 <simontimnan@gmail.com>
2024-09-29 16:54:16 -0700

    update

--- src/Home.jsx
+++ src/Home.jsx
@@ -14,8 +14,7 @@ const Home = () => {
     const newBlogs = blogs.filter(blog => blog.id !== id)
     setBlogs(newBlogs)
   }
-
-   
+ 
   return (
     <div className='home'>
     <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
